2dd364217112b78394ae5fb2189e0ca3344319cc
Andrey Solovyev (work) <andreysoloweb@gmail.com>
2014-03-26 16:56:48 +0400

    fix

---
+++ static/work/js/template.js
@@ -0,0 +1,21 @@
+/**
+ * Created with WebStorm.
+ * User: ASolovyev@dir.condenast.ru
+ * Date: 3/26/14
+ * Time: 4:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+$(function() {
+    /*
+    $('.middle .container').height($(window).height()-197);
+    $('.main_slide').height($(window).height()-197);
+    $(window).resize(function() {
+        console.log('resize');
+        $('.middle .container').height($(window).height()-197);
+        $('.main_slide').height($(window).height()-197);
+    });
+    */
+    //console.log('dd');
+    //$('body').addClass('hello');
+
+});
